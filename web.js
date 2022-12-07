c3456fab6a0b6f896d0192732ee6f58381fa0998
prncss130917 <prncssmauricio130917@gmail.com>
2022-12-07 16:06:06 +0800

    created Fname-Lname html file with js

--- web.js
+++ web.js
@@ -1,3 +1,29 @@
 // my first js here
 
-document.write('successfully attached an external JavaScript file');
+document.getElementById("demo1").innerHTML = "My First JavaScript";
+
+// To access an HTML element
+// The id attribute defines the HTML element
+// innerHTML property defines the HTML content
+function myFunction() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed!";
+  }
+
+
+  // Writing into the HTML output using document.write().
+
+
+
+// To access an HTML element
+  document.getElementById("demo3").innerHTML = 3 + 9 + 5 + 6;
+
+
+
+// For testing purposes, it is convenient to use document.write():
+  document.write(100 * 4);
+
+//you can skip window keyword
+  // window.alert(10 * 10);
+
+// debugging purposes, you can call the console.log()
+  console.log(10 + 9);
