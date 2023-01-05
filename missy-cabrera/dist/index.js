d1314e90c31bf49ab2d17bb2901d9c199027b7d3
missy cabrera <missymyrrchloe05@gmail.com>
2023-01-05 13:17:53 +0700

    Static

--- missy-cabrera/dist/index.js
+++ missy-cabrera/dist/index.js
@@ -96,4 +96,17 @@ class SeatAssignment {
 let seats = new SeatAssignment();
 seats.A1 = 'missy';
 seats.A2 = 'may';
+class Ride {
+    start() { Ride._activeRides++; }
+    stop() { Ride._activeRides--; }
+    static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+Ride._activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index.js.map
--- missy-cabrera/src/index.ts
+++ missy-cabrera/src/index.ts
@@ -176,3 +176,20 @@ class SeatAssignment {
 let seats = new SeatAssignment();
 seats.A1 = 'missy';
 seats.A2 = 'may';
+//Static_members
+ class Ride {
+    private static _activeRides: number = 0;
+    start() { Ride._activeRides++;}
+    stop() { Ride._activeRides--;}
+   
+    static get activeRides() {
+        return Ride._activeRides;
+    }
+}
+ let ride1= new Ride();
+ ride1.start();
+
+ let ride2 = new Ride();
+ ride2.start();
+
+ console.log(Ride.activeRides);
